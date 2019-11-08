6838f88500770734cfa4f0373dbd0bdd5856d650
aldillek <aldillek@gmail.com>
2019-11-08 10:44:06 +0100

    Changes better view site.

--- js/js.js
+++ js/js.js
@@ -1,21 +1,12 @@
-$(document).ready(function(){
-	//basic slider
-	$(".image_next").on('click', function(){
-		var activeImage = $("img.active");
-		var nextImage = activeImage.next();
-		if(nextImage.length){
-			activeImage.removeClass("active");
-			nextImage.addClass("active");
-		}
-	})
-	$(".image_back").on('click', function(){
-		var activeImage = $("img.active");
-		var prevImage = activeImage.prev();
-		if(prevImage.length){
-			activeImage.removeClass("active");
-			prevImage.addClass("active");
-		}
-	})
+let oldPos = window.pageYOffset || document.documentElement.scrollTop; //init old variable to get first distance
+window.addEventListener("scroll", function () {
+	let newPos = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+	if (oldPos < newPos) {
+		document.getElementsByClassName("menu-header")[0].style.top = "-80px";
+	} else {
+		document.getElementsByClassName("menu-header")[0].style.top = "0px";
+	}
+	oldPos = newPos <= 0 ? 0 : newPos;// For Mobile or negative scrolling
+}, false);
 
-
-})
+//|| (oldPos-newPos) >= 1)
