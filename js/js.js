3cae0d180ebf33bc4b176e7c27c099c29f9b1efe
aldillek <aldillek@gmail.com>
2020-02-11 16:32:10 +0100

    So much changes, better mobile look, and a lot of optimizations.

--- js/js.js
+++ js/js.js
@@ -1,12 +1,26 @@
-let oldPos = window.pageYOffset || document.documentElement.scrollTop; //init old variable to get first distance
+let oldPos = window.pageYOffset || document.documentElement.scrollTop;
 window.addEventListener("scroll", function () {
 	let newPos = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-	if (oldPos < newPos) {
-		document.getElementsByClassName("menu-header")[0].style.top = "-80px";
+	if (oldPos > newPos || newPos < 80) {
+		document.querySelector('.menu').classList.remove('menu--hide');
 	} else {
-		document.getElementsByClassName("menu-header")[0].style.top = "0px";
+		document.querySelector('.menu').classList.add('menu--hide');
 	}
 	oldPos = newPos <= 0 ? 0 : newPos;// For Mobile or negative scrolling
 }, false);
 
-//|| (oldPos-newPos) >= 1)
+document.querySelectorAll('.foot__text--href, .menu__item').forEach(button => {
+	button.addEventListener('click', button => {
+		const sectonTop = document.querySelector(`.${button.currentTarget.name}`).offsetTop;
+		window.scroll(0, sectonTop);
+		if (window.screen.width < 480) {
+			document.querySelector('.menu').classList.toggle('menu--show');
+		}
+	})
+});
+
+
+const menuButton = document.querySelector('.menu__logo');
+menuButton.addEventListener('click', () => {
+	document.querySelector('.menu').classList.toggle('menu--show');
+});
